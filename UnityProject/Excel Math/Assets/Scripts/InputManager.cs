fbc1869ac1ec790044f8710532ea91d1354485dd
Rui Xie <siri.ruix@gmail.com>
2019-07-10 14:41:54 -0700

    Update standard assets;

--- UnityProject/Excel Math/Assets/Scripts/InputManager.cs
+++ UnityProject/Excel Math/Assets/Scripts/InputManager.cs
@@ -30,6 +30,12 @@ public class InputManager : MonoBehaviour {
     private Vector3[]           handPositions = new Vector3[2];
     private Vector3[]           handPositionsLastFrame = new Vector3[2];
 
+    private AudioSource         audioSource;
+
+    [SerializeField]
+    private AudioClip           handDetectedClip;
+    private AudioClip           handLostClip;
+
     void GestureEvent_Tapped(TappedEventArgs args) {
         Debug.Log("Tapped");
         if (selectedObject) {
@@ -63,12 +69,38 @@ public class InputManager : MonoBehaviour {
         }
     }
 
+    private void GestureEvent_ManipulationUpdated(ManipulationUpdatedEventArgs args) {
+        Debug.Log("Manipulation Updated");
+        Debug.Log(args.source.kind);
+    }
+
+    private void GestureEvent_ManipulationStarted(ManipulationStartedEventArgs args) {
+        Debug.Log("Manipulation Started");
+    }
+
+    private void GestureEvent_ManipulationCompleted(ManipulationCompletedEventArgs args) {
+        Debug.Log("Manipulation Completed");
+    }
+
+    private void GestureEvent_NavigationUpdated(NavigationUpdatedEventArgs args) {
+        Debug.Log("Navigation Updated");
+        Debug.Log(args.source.handedness);
+    }
+
+    private void GestureEvent_NavigationStarted(NavigationStartedEventArgs args) {
+        Debug.Log("Navigation Started");
+    }
+
+    private void GestureEvent_NavigationCompleted(NavigationCompletedEventArgs args) {
+        Debug.Log("Navigation Completed");
+    }
+
     private void InteractionSourceUpdated(InteractionSourceUpdatedEventArgs args) {
         Vector3 pos;
         handPositionsLastFrame[0] = handPositions[0];
         handPositionsLastFrame[1] = handPositions[1];
         if (args.state.sourcePose.TryGetPosition(out pos)) {
-            if(args.state.source.handedness == InteractionSourceHandedness.Left) {
+            if (args.state.source.handedness == InteractionSourceHandedness.Left) {
                 handPositions[0] = pos;
             }
             else if (args.state.source.handedness == InteractionSourceHandedness.Right) {
@@ -77,20 +109,16 @@ public class InputManager : MonoBehaviour {
         }
     }
 
-    private void GestureEvent_ManipulationUpdated(ManipulationUpdatedEventArgs args) {
-        Debug.Log("Manipulation Updated");
+    private void InteractionSourceDetected(InteractionSourceDetectedEventArgs args) {
+        Debug.Log("Interaction Source Detected");
+        audioSource.PlayOneShot(handDetectedClip);
+
     }
 
-    private void GestureEvent_ManipulationStarted(ManipulationStartedEventArgs args) {
-        Debug.Log("Manipulation Started");
-    }
+    private void InteractionSourceLost(InteractionSourceLostEventArgs args) {
+        Debug.Log("Interaction Source Lost");
+        audioSource.PlayOneShot(handLostClip);
 
-    private void GestureEvent_NavigationUpdated(NavigationUpdatedEventArgs args) {
-        Debug.Log("Navigation Updated");
-    }
-
-    private void GestureEvent_NavigationStarted(NavigationStartedEventArgs args) {
-        Debug.Log("Navigation Started");
     }
 
     void OnDrawGizmos() {
@@ -103,6 +131,8 @@ public class InputManager : MonoBehaviour {
 
     void Awake() {
         InteractionManager.InteractionSourceUpdated += InteractionSourceUpdated;
+        InteractionManager.InteractionSourceDetected += InteractionSourceDetected;
+        InteractionManager.InteractionSourceLost += InteractionSourceLost;
 
         recognizer = new GestureRecognizer();
         recognizer.SetRecognizableGestures(GestureSettings.Tap | GestureSettings.Hold | GestureSettings.DoubleTap | GestureSettings.ManipulationTranslate);
@@ -112,9 +142,16 @@ public class InputManager : MonoBehaviour {
         recognizer.HoldCompleted += GestureEvent_HoldCompleted;
         recognizer.ManipulationStarted += GestureEvent_ManipulationStarted;
         recognizer.ManipulationUpdated += GestureEvent_ManipulationUpdated;
-        recognizer.NavigationStarted
+        recognizer.ManipulationCompleted += GestureEvent_ManipulationCompleted;
+        recognizer.NavigationStarted += GestureEvent_NavigationStarted;
+        recognizer.NavigationUpdated += GestureEvent_NavigationUpdated;
+        recognizer.NavigationCompleted += GestureEvent_NavigationCompleted;
 
         recognizer.StartCapturingGestures();
+
+        audioSource = GetComponent<AudioSource>();
+        handDetectedClip = Resources.Load<AudioClip>("Audio/Jump");
+        handLostClip = Resources.Load<AudioClip>("Audio/Land");
     }
 
     void Start() {
